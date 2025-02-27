d1a5738997c6acca3b081b7599dc862f31ac8659
kushagrashri7 <kushagrashrisharma@gmail.com>
2025-02-27 15:47:17 +0530

    [Kushagra] Add.FirstSpringApp UC01

---
+++ src/main/java/com/firstspringapp/HelloController.java
@@ -0,0 +1,16 @@
+package com.firstspringapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    //UC-1
+    @GetMapping("/hello1")
+    public String sayHello1(){
+        return "Hello from BridgeLabz";
+    }
+}
